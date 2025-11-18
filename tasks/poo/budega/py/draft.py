7f44f09dbf04075de2d8411ed3d14a7ff9fa8fb2
kauafelixcampelo-coder <kauafelixcampelo@gmail.com>
2025-11-18 14:39:50 +0000

    budega

--- tasks/poo/budega/py/draft.py
+++ tasks/poo/budega/py/draft.py
@@ -1,16 +1,40 @@
 class cliente:
     def _init_(self, nome:str):
-        self._nome = nome
+        self.__nome = nome
     
     def getnome(self) -> str:
         return self.__nome
     
     def _str_(self) -> str:
-        return f"{self._nome}"
+        return f"{self.__nome}"
     
 class Mercantil:
-    def _init_(self, qtdcaixa:int):
-        self.caixa = qtdcaixa
-        self.qtdcaixa = [None] * qtdcaixaself.fila = [None]
+    def __init__(self, qtdcaixa:int):
+        self.caixa: list[cliente | None] = [None] * qtdcaixa
+        self.qtdcaixa = qtdcaixa
+        self.fila: list[cliente | None] = []
+
+    def __str__(self) -> str:
+        caixas: list[str] = list(map(lambda cliente: '-----' if cliente is None else cliente.getnome(), self.caixa))
+        strCaixasFinal: str = "[" +", ".join(caixas) + "]"
+        fila: list[str] = list(map(lambda p: "" if p is None else p.getnome(), self.fila))
+        strFilaFinal: str = "[" +", ".join(fila) + "]"
+        return f"Caixas: {strCaixasFinal}\nEspera: {strFilaFinal}"
+    
+    def chegar (self, nome: cliente | None) -> None:
+        self.fila.append(nome)
+        return
+    
+    def chamar(self, number: int):
+        if not self.fila:
+            print("fail: sem cliente")
+            return
         
+    
+
         
+
+
+
+
+
